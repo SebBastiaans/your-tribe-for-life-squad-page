466a350a4463cf1bdbce124920090c50f089210c
EmreBahar0912 <e.bahar906@gmail.com>
2026-09-06 14:38:58 +0200

    Styling given to img's #10
    Width & height of the img placed inside the HTML element instead of CSS file, for a better performance.

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -2,15 +2,29 @@
     let {data} = $props();
 
     const persons = data.persons;
-    // const avatar = data.avatar;
     const mugshot = data.mugshot;
 </script>
 
 <section>
 {#each persons as person}
     <article>
-        <img src="https://fdnd.directus.app/assets/{person.mugshot}">
+        <img src="https://fdnd.directus.app/assets/{person.mugshot}" width="175px" height="125px">
+        <div>
+            <div></div>
+            <span>Live</span>
+        </div>
         <p>{person.name}</p>
     </article>
 {/each}
 </section>
+
+<style>
+    section {
+        article {
+            img {
+                max-width: 100%;
+                height: auto;
+            }
+        }
+    }
+</style>
