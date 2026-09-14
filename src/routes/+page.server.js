ce101a9e088f4ed0dd8526477eb51ac40e5320f4
Hexterty46 <efsuslusemih@gmail.com>
2026-09-14 15:04:17 +0200

    Component made for the detailpage (only on mobile)
    #4

--- src/routes/+page.server.js
+++ src/routes/+page.server.js
@@ -3,5 +3,8 @@ export async function load(){
     const res = await fetch("https://fdnd.directus.app/items/person?fields=*&filter[squads][squad_id][cohort][_eq]=2627");
     const data = await res.json();
 
-    return { persons: data.data };
+    const squadRes = await fetch(`https://fdnd.directus.app/items/squad/23?fields=*`);
+    const squadData = await squadRes.json();
+
+    return { persons: data.data, squad: squadData.data };
 }
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,109 +1,138 @@
 <script>
-    let {data} = $props();
+  import PersonDetail from "$lib/PersonDetail.svelte"
+  import shrek from "$lib/assets/Shrek.webp"
 
-    import shrek from '$lib/assets/Shrek.webp';
+  let { data } = $props()
 
-    const persons = data.persons;
+  const persons = data.persons
+  const squad = data.squad
+  
+  let chosenPerson = $state(null)
+
+  async function choosePerson(id) {
+    const response = await fetch(
+      `https://fdnd.directus.app/items/person/${id}?fields=*`,
+    )
+
+    const result = await response.json()
+
+    chosenPerson = result.data
+  }
 </script>
 
-<section>
-    {#each persons as person}
-        <a href={`/detail/${person.id}`}>
-            <article>
-            {#if person.mugshot}
-                <img src="https://fdnd.directus.app/assets/{person.mugshot}" width="225" height="175" alt="Mugshot van {person.name}">
-            {:else}
-                <img src="{shrek}" width="225" height="175" alt="Mugshot van Shrek">
-            {/if}
-                <p>Live</p>
-                <h2>{person.name}</h2>
-            </article>
-        </a>
-    {/each}
+<main>
+  {#if chosenPerson}
+    <PersonDetail person={chosenPerson} squad={data.squad} />
+  {:else}
+    <p>Nog niet op een persoon geklikt</p>
+  {/if}
+</main>
+
+<section class="person-list">
+  {#each persons as person}
+    <button on:click={() => choosePerson(person.id)}>
+      <article>
+        {#if person.mugshot}
+          <img
+            src={`https://fdnd.directus.app/assets/${person.mugshot}`}
+            alt={`Mugshot van ${person.name}`}
+          />
+        {:else}
+          <img src={shrek} alt="Mugshot van Shrek" />
+        {/if}
+
+        <p>Live</p>
+        <h2>{person.name}</h2>
+      </article>
+    </button>
+  {/each}
 </section>
 
 <style>
-    section {
-        display: flex;
-        position: fixed;
-        bottom: 0;
-        left: 0;
-        right: 0;
-        gap: 1em;
-        /* margin: 1em; */
-        overflow-x: auto;
-        scroll-behavior: smooth;
-        background-color: var(--secondary-text-color);
-        padding: 1rem;
+  section {
+    display: flex;
+    position: fixed;
+    bottom: 0;
+    left: 0;
+    right: 0;
+    gap: 1em;
+    /* margin: 1em; */
+    overflow-x: auto;
+    scroll-behavior: smooth;
+    background-color: #181a1f;
+    padding: 1rem;
 
-        a {
-            display: block;
-            /* width: 175px; */
-            flex: 0 0 175px;
-            color: inherit;
-            text-decoration: none;
+    > button {
+      display: block;
+      /* width: 175px; */
+      border: none;
+      background-color: transparent;
+      flex: 0 0 175px;
+      text-align: inherit;
+      color: inherit;
+      text-decoration: none;
+    }
+
+    article {
+      position: relative;
+      width: 100%;
+      flex: 0 0 175px;
+      margin-block-start: 1rem;
+
+      img {
+        width: 100%;
+        height: 100px;
+        object-fit: cover;
+        display: block;
+        border-radius: 12px;
+      }
+
+      > p {
+        display: flex;
+        align-items: center;
+        position: absolute;
+        gap: 0.3em;
+        width: fit-content;
+        padding-inline: 6px;
+        padding-block: 4px;
+        border-radius: var(--radius-round);
+        background: black;
+        top: 0.5em;
+        left: 0.5em;
+        font-size: 14px;
+
+        &:before {
+          content: "";
+          display: block;
+          width: 8px;
+          height: 8px;
+          background-color: var(--primary-accent-color);
+          border-radius: 50%;
         }
 
-        article {
-            position: relative;
-            width: 100%;
-            flex: 0 0 175px;
-            margin-block-start: 1rem;
-
-            img {
-                width: 100%;
-                height: 100px;
-                object-fit: cover;
-                display: block;
-                border-radius: 12px;
-            }
-
-            > p {
-                display: flex;
-                align-items: center;
-                position: absolute;
-                gap: .3em;
-                width: fit-content;
-                padding-inline: 6px;
-                padding-block: 4px;
-                border-radius: var(--radius-round);
-                background: black;
-                top: .5em;
-                left: .5em;
-                font-size: 14px;
-
-                &:before {
-                    content: '';
-                    display: block;
-                    width: 8px;
-                    height: 8px;
-                    background-color: var(--primary-accent-color);
-                    border-radius: 50%;
-                }
-
-                /* span {
+        /* span {
                     color: var(--primary-text-color);
                     font-family: 'inter';
                     font-size: 14px;
                 } */
-            }
+      }
 
-            h2 {
-                position: absolute;
-                background: rgb(0 0 0 / 70%);
-                color: var(--primary-text-color);
-                font-family: 'inter';
-                font-weight: 500;
-                text-align: center;
-                font-size: 20px;
-                bottom: 0;
-                right: 0;
-                left: 0;
-                border-radius: 0 0 12px 12px;
-                white-space: nowrap;
-                overflow: hidden;
-                text-overflow: ellipsis;
-            }
-        }
+      h2 {
+        position: absolute;
+        background: rgb(0 0 0 / 70%);
+        color: var(--primary-text-color);
+        font-family: "inter";
+        font-weight: 500;
+        text-align: center;
+        font-size: 20px;
+        bottom: 0;
+        right: 0;
+        left: 0;
+        border-radius: 0 0 12px 12px;
+        white-space: nowrap;
+        overflow: hidden;
+        text-overflow: ellipsis;
+      }
     }
+  }
 </style>
